7a050052c9cfd340f7136507d9de74f0c1cfb70a
Pavel <p.yudenko@gmail.com>
2022-06-16 22:08:01 +0300

    home work: automatic testing, les.2 ver.2

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -3,5 +3,5 @@ import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
 assert.equal(capitalize(''), '');
-assert.equal(capitalize('jello'), 'Hello');
+assert.equal(capitalize('hello'), 'Hello');
 console.log('All tests is passed!')
