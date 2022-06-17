b547d6ca86b7a60b9be82d6f4a8cc79db4dcf873
Pavel <p.yudenko@gmail.com>
2022-06-17 13:16:58 +0300

    delited tests/capitalize.test.js

--- tests/capitalize.test.js
+++
@@ -1,9 +0,0 @@
-import {capitalize} from '../src/capitalize.js';
-
-if (capitalize('hello') !== 'Hello') {
-  throw new Error ('Function is not working properly');
-}
-if (capitalize('') !== '') {
-  throw new Error ('Function is not working properly')
-}
-console.log('All tests is passed!')
