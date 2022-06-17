f4592242dcb9a4ede92bff6ff83a2e93e2d7dc48
Pavel <p.yudenko@gmail.com>
2022-06-17 13:20:29 +0300

    rename to _test/capitalize.test.js

--- test/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-
-import assert from 'power-assert';
-import capitalize from '../src/capitalize.js';
-
-assert(capitalize('') === '');
-assert(capitalize('hello') === 'Hello');
-console.log('All tests is passed!')
